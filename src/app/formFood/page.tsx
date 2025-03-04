844d29f3800188237a62fb3044ebec6b905f7730
Farhan Maulana Pangestu <136353177+fmpangestu@users.noreply.github.com>
2025-03-04 07:58:17 +0700

    update color theme

--- src/app/formFood/page.tsx
+++ src/app/formFood/page.tsx
@@ -214,166 +214,172 @@ const FoodRecommendation = () => {
   };
 
   return (
-    <div className="container mx-auto p-6">
-      <h1 className="text-2xl font-bold mb-4">Rekomendasi Makanan Sehat</h1>
-      <form className="space-y-4" onSubmit={handleSubmit}>
-        <div>
-          <label className="block mb-2" htmlFor="weight">
-            Berat Badan (kg)
-          </label>
-          <input
-            type="number"
-            id="weight"
-            name="weight"
-            value={formData.weight}
-            onChange={handleInputChange}
-            className="text-black border p-2 w-full"
-            required
-          />
-        </div>
+    <>
+      <div className="md:container mx-2 md:mx-auto p-6 text-white bg-[#00712D] mt-4 rounded-xl">
+        <h1 className="text-2xl font-bold mb-4">Rekomendasi Makanan Sehat</h1>
+        <form className="space-y-4" onSubmit={handleSubmit}>
+          <div>
+            <label className="block mb-2" htmlFor="weight">
+              Berat Badan (kg)
+            </label>
+            <input
+              type="string"
+              id="weight"
+              name="weight"
+              value={formData.weight}
+              onChange={handleInputChange}
+              className="text-[#0d1821] bg-white outline-[#0d1821] rounded-lg p-2 w-full"
+              required
+            />
+          </div>
 
-        <div>
-          <label className="block mb-2" htmlFor="height">
-            Tinggi Badan (cm)
-          </label>
-          <input
-            type="number"
-            id="height"
-            name="height"
-            value={formData.height}
-            onChange={handleInputChange}
-            className="text-black border p-2 w-full"
-            required
-          />
-        </div>
+          <div>
+            <label className="block mb-2" htmlFor="height">
+              Tinggi Badan (cm)
+            </label>
+            <input
+              type="string"
+              id="height"
+              name="height"
+              value={formData.height}
+              onChange={handleInputChange}
+              className="text-[#0d1821] outline-[#0d1821] rounded-lg p-2 w-full"
+              required
+            />
+          </div>
 
-        <div>
-          <label className="block mb-2" htmlFor="age">
-            Usia (tahun)
-          </label>
-          <input
-            type="number"
-            id="age"
-            name="age"
-            value={formData.age}
-            onChange={handleInputChange}
-            className="text-black border p-2 w-full"
-            required
-          />
-        </div>
+          <div>
+            <label className="block mb-2" htmlFor="age">
+              Usia (tahun)
+            </label>
+            <input
+              type="string"
+              id="age"
+              name="age"
+              value={formData.age}
+              onChange={handleInputChange}
+              className="text-[#0d1821] outline-[#0d1821] rounded-lg p-2 w-full"
+              required
+            />
+          </div>
 
-        <div>
-          <label className="block mb-2" htmlFor="gender">
-            Jenis Kelamin
-          </label>
-          <select
-            id="gender"
-            name="gender"
-            value={formData.gender}
-            onChange={handleInputChange}
-            className="text-black border p-2 w-full"
-          >
-            <option value="male">Laki-laki</option>
-            <option value="female">Perempuan</option>
-          </select>
-        </div>
+          <div>
+            <label className="block mb-2" htmlFor="gender">
+              Jenis Kelamin
+            </label>
+            <select
+              id="gender"
+              name="gender"
+              value={formData.gender}
+              onChange={handleInputChange}
+              className="text-[#0d1821] outline-[#0d1821] rounded-lg p-2 w-full"
+            >
+              <option value="male">Laki-laki</option>
+              <option value="female">Perempuan</option>
+            </select>
+          </div>
 
-        <div>
-          <label className="block mb-2" htmlFor="activityLevel">
-            Tingkat Aktivitas
-          </label>
-          <select
-            id="activityLevel"
-            name="activityLevel"
-            value={formData.activityLevel}
-            onChange={handleInputChange}
-            className="text-black border p-2 w-full"
-          >
-            <option value="sedentary">Sedentary (Minim aktivitas)</option>
-            <option value="light">
-              Ringan (Olahraga ringan 1-3 hari/minggu)
-            </option>
-            <option value="moderate">
-              Sedang (Olahraga moderat 3-5 hari/minggu)
-            </option>
-            <option value="active">
-              Aktif (Olahraga intensif 6-7 hari/minggu)
-            </option>
-          </select>
-        </div>
+          <div>
+            <label className="block mb-2" htmlFor="activityLevel">
+              Tingkat Aktivitas
+            </label>
+            <select
+              id="activityLevel"
+              name="activityLevel"
+              value={formData.activityLevel}
+              onChange={handleInputChange}
+              className="text-[#0d1821] outline-[#0d1821] rounded-lg p-2 w-full"
+            >
+              <option value="sedentary">Sedentary (Minim aktivitas)</option>
+              <option value="light">
+                Ringan (Olahraga ringan 1-3 hari/minggu)
+              </option>
+              <option value="moderate">
+                Sedang (Olahraga moderat 3-5 hari/minggu)
+              </option>
+              <option value="active">
+                Aktif (Olahraga intensif 6-7 hari/minggu)
+              </option>
+            </select>
+          </div>
 
-        {error && (
-          <p className="absolute top-0 w-[80%] text-red-500 px-2 py-1 rounded-lg">
-            {error}
-          </p>
-        )}
-
-        <button
-          type="submit"
-          className="bg-green-500 font-semibold w-full text-white py-2 px-4 rounded"
-          disabled={loading}
-        >
-          {loading ? "Loading..." : "Submit"}
-        </button>
-      </form>
-
-      {idealWeight && (
-        <div className="mt-4 bg-white text-gray-700 rounded-lg p-4 ">
-          <h2 className="text-xl font-semibold">
-            Berat Badan IdealMu: {idealWeight}
-          </h2>
-          {success && (
-            <p className=" w-full text-white bg-green-500 px-2 py-1 rounded-lg">
-              Note: {success}
+          {error && (
+            <p className="absolute top-0 w-[80%] text-red-500 px-2 py-1 rounded-lg">
+              {error}
             </p>
           )}
-          <div className="grid grid-cols-1 sm:grid-cols-2">
-            <h3 className="mt-2">Kalori HarianMu: {calorieNeeds}kcal</h3>
-            <h3 className="mt-2">Protein HarianMu: {dailyProteinNeeds}g</h3>
-            <h3 className="mt-2">Gula HarianMu: {dailySugarNeeds}g</h3>
-            <h3 className="mt-2">Sodium HarianMu: {dailySodiumNeeds}g</h3>
-            <h3 className="mt-2">Karbohidrat HarianMu: {dailyCarbsNeeds}g</h3>
-            <h3 className="mt-2">
-              Batas Lemak Jenuh HarianMu: {saturedFatLimit}g
-            </h3>
-          </div>
-          {/* {success && <p className="mt-2 text-green-500">{success}</p>} */}
-        </div>
-      )}
 
-      {foods.length > 0 && (
-        <div className="mt-4">
-          <h3 className="text-xl font-semibold">Rekomendasi Makanan:</h3>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-2">
-            {foods.map((food, index) => (
-              <div key={index} className="border border-white rounded-md p-4">
-                <h2 className="font-semibold text-lg">{food.name}</h2>
-                <p>Deskripsi: {food.description}</p>
-                <p>Persiapan: {food.preparation}</p>
-                <p>porsi: {food.portion_weight}g</p>
-                <p>Kategori: {food.category}</p>
-                <h4>Nutrisi</h4>
-                <ul>
-                  <li>Protein: {food.protein} g</li>
-                  <p>Kalori: {food.calories} kcal</p>
-                  <li>fat: {food.fat} g</li>
-                  <li>Carbs: {food.carbs} g</li>
-                  <li>Serving Size: {food.serving_size}</li>
-                </ul>
-                <h4>Resep:</h4>
-                <ul className="list-disc ">
-                  {food.ingredients.map((ingredient, index) => (
-                    <li key={index} className="list-inside">
-                      {ingredient}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ))}
+          <button
+            type="submit"
+            className="bg-[#D5ED9F] font-semibold w-full text-[#00712D] py-2 px-4 rounded"
+            disabled={loading}
+          >
+            {loading ? "Loading..." : "Submit"}
+          </button>
+        </form>
+      </div>
+      <div className="md:container mx-2 md:mx-auto mb-5 rounded-xl">
+        {idealWeight && (
+          <div className="mt-4 bg-[#D5ED9F] text-[#00712D] rounded-lg p-4 ">
+            <h2 className="text-xl font-semibold">
+              Berat Badan IdealMu: {idealWeight}
+            </h2>
+            {success && (
+              <p className=" w-full text-[#FF9100] bg-[#FFFBE6] px-2 py-1 rounded-lg">
+                Note: {success}
+              </p>
+            )}
+            <div className="grid grid-cols-1 sm:grid-cols-2">
+              <h3 className="mt-2">Kalori HarianMu: {calorieNeeds}kcal</h3>
+              <h3 className="mt-2">Protein HarianMu: {dailyProteinNeeds}g</h3>
+              <h3 className="mt-2">Gula HarianMu: {dailySugarNeeds}g</h3>
+              <h3 className="mt-2">Sodium HarianMu: {dailySodiumNeeds}g</h3>
+              <h3 className="mt-2">Karbohidrat HarianMu: {dailyCarbsNeeds}g</h3>
+              <h3 className="mt-2">
+                Batas Lemak Jenuh HarianMu: {saturedFatLimit}g
+              </h3>
+            </div>
+            {/* {success && <p className="mt-2 text-green-500">{success}</p>} */}
           </div>
-        </div>
-      )}
-    </div>
+        )}
+
+        {foods.length > 0 && (
+          <div className="mt-4">
+            <h3 className="text-xl font-semibold">Rekomendasi Makanan:</h3>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-2">
+              {foods.map((food, index) => (
+                <div
+                  key={index}
+                  className="border border-[#ffffff] rounded-md p-4"
+                >
+                  <h2 className="font-semibold text-lg">{food.name}</h2>
+                  <p>Deskripsi: {food.description}</p>
+                  <p>Persiapan: {food.preparation}</p>
+                  <p>porsi: {food.portion_weight}g</p>
+                  <p>Kategori: {food.category}</p>
+                  <h4>Nutrisi</h4>
+                  <ul>
+                    <li>Protein: {food.protein} g</li>
+                    <p>Kalori: {food.calories} kcal</p>
+                    <li>fat: {food.fat} g</li>
+                    <li>Carbs: {food.carbs} g</li>
+                    <li>Serving Size: {food.serving_size}</li>
+                  </ul>
+                  <h4>Resep:</h4>
+                  <ul className="list-disc ">
+                    {food.ingredients.map((ingredient, index) => (
+                      <li key={index} className="list-inside">
+                        {ingredient}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
