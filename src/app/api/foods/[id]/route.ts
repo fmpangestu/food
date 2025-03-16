e269152632f7af96ed9fd6e6588171a16d66c278
Farhan Maulana Pangestu <136353177+fmpangestu@users.noreply.github.com>
2025-03-17 00:41:21 +0700

    update data in csv to here

--- src/app/api/foods/[id]/route.ts
+++ src/app/api/foods/[id]/route.ts
@@ -37,7 +37,10 @@ export async function PUT(request: NextRequest) {
     }
 
     const updatedFood = await request.json();
-    const success = await updateFood(decodeURIComponent(id), updatedFood);
+    const success = await updateFoodInServer(
+      decodeURIComponent(id),
+      updatedFood
+    );
 
     if (success) {
       return NextResponse.json({ success: true, food: updatedFood });
@@ -60,7 +63,7 @@ export async function DELETE(request: NextRequest) {
     }
 
     console.log(`API: Mencoba menghapus makanan dengan id "${id}"`);
-    const success = await deleteFood(decodeURIComponent(id));
+    const success = await deleteFoodFromServer(decodeURIComponent(id));
 
     if (success) {
       return NextResponse.json({ success: true });
